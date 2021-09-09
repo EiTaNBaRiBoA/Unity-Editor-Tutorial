af9e320835ff97da73b0391c47ba31440c8989f2
EiTaNBaRiBoA <eitanshapirshtein@gmail.com>
2021-09-09 12:46:20 +0300

    Implemented preview window

--- Assets/SphereEditor.cs
+++ Assets/SphereEditor.cs
@@ -8,10 +8,21 @@ using UnityEngine;
 public class SphereEditor : Editor
 {
 
-    SerializedObject so;
+    SerializedObject so;//the current selected SphereSO object
+
+
+    // property fields of the sphereSO
     SerializedProperty propColor;
     SerializedProperty go, goName;
     SerializedProperty x, y, z;
+
+
+    #region Preview Of the gameobject
+
+    Editor gameObjectEditor;
+    GUIStyle bgColor = new GUIStyle(); // The background of the preview
+    SphereSO sO; // Caching the SphereSo
+    #endregion
     private void OnEnable()
     {
         so = serializedObject;
@@ -21,9 +32,13 @@ public class SphereEditor : Editor
         z = so.FindProperty("z");
         go = so.FindProperty("gameObject");
         goName = so.FindProperty("gameobjectName");
+        sO = (SphereSO)so.targetObject; // Casting the serializedobject into the cached so
+        bgColor.normal.background = EditorGUIUtility.whiteTexture;
     }
     public override void OnInspectorGUI()
     {
+
+        // Drawing the cells
         so.Update();
         EditorGUILayout.BeginHorizontal();
         EditorGUILayout.BeginVertical();
@@ -37,7 +52,15 @@ public class SphereEditor : Editor
         EditorGUILayout.PropertyField(z);
         EditorGUILayout.EndVertical();
         EditorGUILayout.EndHorizontal();
+
+        // End of the drawing cells
+
+
         so.ApplyModifiedProperties();
+        //Applying Changes of the fields when finished
+
+
+        //Apply Settings into the gameobjects
         if (GUILayout.Button("Apply Properties"))
         {
             foreach (Sphere sphere in FindObjectsOfType<Sphere>())
@@ -45,12 +68,24 @@ public class SphereEditor : Editor
                 sphere.ApplyModification();
             }
         }
+        //Apply Settings into the Editor Instantiate
         if (GUILayout.Button("Instantiate"))
         {
             foreach (Sphere sphere in FindObjectsOfType<Sphere>())
             {
                 sphere.instantiateNewObject();
             }
+        }
+
+
+        //Creation the of the preview window to see changes before implementing them
+        GameObject gameObject = (GameObject)EditorGUILayout.ObjectField(sO.gameObject, typeof(GameObject), true);
+        if (gameObject != null)
+        {
+            gameObject.GetComponent<Renderer>().sharedMaterial.SetColor("_Color", sO.color);
+            if (gameObjectEditor == null)
+                gameObjectEditor = Editor.CreateEditor(gameObject);
+            gameObjectEditor.OnInteractivePreviewGUI(GUILayoutUtility.GetRect(256, 256), bgColor);
         }
     }
 }
